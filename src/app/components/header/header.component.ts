c31a1757e13982094fd2f31bb2c18d2df6d33071
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-06-19 23:39:56 +0200

    El header ya muestra las categorias de los posts y me manda a la ruta correspondiente

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/interfaces/category';
+import { CategoriasService } from 'src/app/services/categorias.service';
 
-@Component({
+@Component( {
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
-})
+  styleUrls: [ './header.component.css' ]
+} )
 export class HeaderComponent implements OnInit {
+  categorias: Category[]
+  constructor ( private categoriasService: CategoriasService ) {
+    this.categorias = this.categoriasService.getAll()
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit (): void {
+    console.log( this.categorias )
   }
 
 }

8ddef3c1b2b886abd7f65f060ae1f5942426909a
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-06-19 14:03:13 +0200

    Creacion de un post

---
+++ src/app/db/posts.db.ts
@@ -0,0 +1,13 @@
+import { Post } from "../interfaces/post";
+
+export const POSTS: Post[] = [
+    {
+        id: 1,
+        titulo: 'Viaje a Suiza',
+        texto: 'Texto del post Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla cumque itaque quae voluptatem asperiores blanditiis nemo quis tempora unde dignissimos dolorem illo quaerat laudantium, recusandae est repellendus. Illum, nesciunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores fugiat voluptatem commodi unde perferendis, et culpa, reprehenderit ea quibusdam, nemo consequuntur ex. Odio consequatur, veniam praesentium minus quia alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem natus maxime deleniti temporibus eos? Illo minus, molestiae quidem voluptatem facere error saepe quis eaque nobis itaque dolores dignissimos, modi quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est earum exercitationem error, molestiae, minima eaque eligendi eveniet pariatur laborum voluptatem rem dicta. Rem, tenetur temporibus cum perspiciatis aliquid quasi. Nihil! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit nulla commodi velit, provident quas non ea laboriosam quae, debitis corrupti perspiciatis amet odio rem minima deserunt voluptates? Nihil, odio iure.',
+        autor: 'Raul',
+        imagen: 'https://wp.themedemo.org/WP01/WP001/wp-content/uploads/2022/04/activity-02.jpg',
+        fecha: '28/2/21',
+        categoria: { id: 1, titulo: 'destacados' }
+    }
+]
